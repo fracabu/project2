49d09a9c28310439f26a5bf051d0b20aa7037f78
fracabu <100199965+fracabu@users.noreply.github.com>
2024-09-01 00:33:09 +0200

    add BacktestModel

--- webapp/model/BacktestModel.js
+++ webapp/model/BacktestModel.js
@@ -10,11 +10,12 @@ sap.ui.define([
                     test: [
                         {
                             description: "EA Backtest - Advisor 1",
+                            winRateTotal: "1.80%",
                             winRateShort: "1.38%",
                             winRateLong: "2.22%",
                             netProfit: 69879.70,
                             maxDrawdownAbsolute: 5235.80,
-                            maxDrawdownPercentage: "7.55%",
+                            maxDrawdownPercentage: "7.53%",
                             profitFactor: 1.61,
                             recoveryFactor: 10.84,
                             sharpeRatio: 6.74,
